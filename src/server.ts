952d6c8ff1086e84b77a772a7568613af14c6953
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-25 17:29:55 -0500

    add logger middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { errors } from "https://deno.land/std@0.183.0/http/http_errors.ts"
 import { toPathString } from "https://deno.land/std@0.184.0/fs/_util.ts"
 import { Application } from "https://deno.land/x/oak@v12.2.0/mod.ts"
+import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts"
 import { port } from "./constants.ts"
 import { errorHandler } from "./error-handler.ts"
 import { layout } from "./layout.ts"
@@ -8,6 +9,8 @@ import { router } from "./router.ts"
 
 const app = new Application()
 
+app.use(logger.logger)
+app.use(logger.responseTime)
 app.use(errorHandler)
 app.use(router.routes())
 app.use(router.allowedMethods())
